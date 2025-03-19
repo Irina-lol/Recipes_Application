4d73bea82ce8cdd2c0bfb6c549270964396c19e3
Irina-lol <irinakazynkina@gmail.com>
2025-03-19 21:39:04 +0700

    Поправила ошибки

--- frontend_app_recipes/src/index.js
+++ frontend_app_recipes/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from "./components/App.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            {' '}
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
