374a8f8606674a46efe2f6f7e07fd7aa2f852576
Irina-lol <irinakazynkina@gmail.com>
2025-04-29 11:44:12 +0700

    исправила настройки

--- app_recipes/app_recipes/settings.py
+++ app_recipes/app_recipes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-is7g)t)@t7jwhkr+7uk!-)^^g!bsh26^69+x#49vbh&yy*4t2y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
